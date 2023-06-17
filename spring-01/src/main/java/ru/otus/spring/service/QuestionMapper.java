508de93e2e13c596f65fc9793c6c19cef3ed0a6c
zybkin <dimonzybkin@mail.ru>
2023-06-17 10:57:10 +0300

    spring-01: QuestionMapper - delete static .* inport

--- spring-01/src/main/java/ru/otus/spring/service/QuestionMapper.java
+++ spring-01/src/main/java/ru/otus/spring/service/QuestionMapper.java
@@ -6,7 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 import static java.lang.System.lineSeparator;
-import static ru.otus.spring.service.CsvReader.*;
+import static ru.otus.spring.service.CsvReader.PARAM_NAME_ID;
+import static ru.otus.spring.service.CsvReader.PARAM_NAME_QUESTION;
+import static ru.otus.spring.service.CsvReader.PARAM_NAME_ANSWERS;
+import static ru.otus.spring.service.CsvReader.PARAM_NAME_RIGHT;
 
 public class QuestionMapper {
     public Question toQuestion(Map<String, String> questionString) {
@@ -14,7 +17,7 @@ public class QuestionMapper {
                 .id(Integer.parseInt(questionString.get(PARAM_NAME_ID)))
                 .text(questionString.get(PARAM_NAME_QUESTION))
                 .answers(parseAnswers(questionString.get(PARAM_NAME_ANSWERS)))
-                .rightAnswer(Integer.parseInt(questionString.get(PARAM_NAME_ID)))
+                .rightAnswer(Integer.parseInt(questionString.get(PARAM_NAME_RIGHT)))
                 .build();
     }
 
